754574ef61cca753b17849e0ac60e773c33a3887
Sebastian <llj03@icloud.com>
2025-09-18 12:08:44 +0200

    Made changes to recipes and finished single-recipe.php
    +Finished single-recipe.php
    +Made design updates to archive-recipe.php
    +Added svg's

--- wp-content/themes/therecipehub/single-recipe.php
+++ wp-content/themes/therecipehub/single-recipe.php
@@ -1,1 +1,51 @@
-dette er nu en single page fra single-recipe
+<?php get_header(); ?>
+
+<main class="single-recipe">
+
+  <!-- Top Section -->
+  <div class="container">
+    <!-- Left side -->
+    <div class="left">
+      <h1><?php the_title(); ?></h1>
+
+      <div class="meta">
+        <span class="meta-item">
+            <img src="/wp-content/uploads/2025/09/clock-solid-full.svg" alt="">
+            <?php echo get_field('total_time'); ?>
+        </span>
+        <br>
+        <span class="meta-item stars">
+            <img src="/wp-content/uploads/2025/09/chart-simple-solid-full.svg" alt="">
+            <?php echo get_field('difficulty'); ?>
+        </span>
+
+      </div>
+
+      <p class="description"><?php echo get_field('intro'); ?></p>
+      <p class="description"><?php echo get_field('description'); ?></p>
+    </div>
+
+    <!-- Right side -->
+    <div class="right">
+      <?php the_post_thumbnail('medium'); ?>
+    </div>
+  </div>
+
+  <!-- Bottom Section -->
+  <div class="bottom">
+    <div class="ingredients">
+      <h2>Ingredients</h2>
+      <p><?php echo nl2br(get_field('ingredients')); ?></p> <!-- Vi havde problem med det stod i en lang snøre, da vi ikke havde adgang til at bruge field typen repeater, fordi den er en pro version ting, desværre. Dog fandt vi ud af, at hvis man bruger nl2br så går den ind og kigger på hvornår brugeren skifter til ny linje og sætter en <br> der, så det går ned på næste linje som ønsket. Vi brugte https://www.w3schools.com/php/func_string_nl2br.asp til det. -->
+    </div>
+
+    <div class="recipe-extra">
+      <h2>Procedure</h2>
+      <p><?php echo nl2br(get_field('procedure')); ?></p>
+    </div>
+  </div>
+
+</main>
+
+<?php get_footer(); ?>
+
+
